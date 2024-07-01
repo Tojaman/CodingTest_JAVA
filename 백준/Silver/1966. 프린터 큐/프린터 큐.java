899414c07e3b17fc4aeb370e7f9d1297e8db636a
조형준 <89668546+Tojaman@users.noreply.github.com>
2024-07-01 15:22:09 +0900

    [Silver III] Title: 프린터 큐, Time: 124 ms, Memory: 14896 KB -BaekjoonHub

---
+++ 백준/Silver/1966. 프린터 큐/프린터 큐.java
@@ -0,0 +1,56 @@
+import java.io.BufferedReader;
+import java.io.IOException;
+import java.io.InputStreamReader;
+import java.util.*;
+
+public class Main {
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        Queue<int[]> q = new LinkedList<int[]>();
+//        PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+        int N = 0;
+        int M = 0;
+
+        int testNum = Integer.parseInt(br.readLine());
+        for (int i = 0; i < testNum; i++) {
+            Queue<int[]> q = new LinkedList<int[]>();
+            PriorityQueue<Integer> pq = new PriorityQueue<>(Collections.reverseOrder());
+            int cnt = 0;
+            StringTokenizer st1 = new StringTokenizer(br.readLine());
+            N = Integer.parseInt(st1.nextToken());
+            M = Integer.parseInt(st1.nextToken());
+
+            int[] priority = new int[N];
+            StringTokenizer st2 = new StringTokenizer(br.readLine());
+            for (int j = 0; j < N; j++) {
+                int num = Integer.parseInt(st2.nextToken());
+                q.add(new int[]{j, num});
+                pq.add(num);
+            }
+            while (!q.isEmpty()) {
+                if (q.peek()[1] == pq.peek()) {
+                    if (q.poll()[0] == M) {
+                        pq.poll();
+                        System.out.println(++cnt);
+                        break;
+                    }
+                    pq.poll();
+                    cnt++;
+                }
+                else
+                    q.add(q.poll());
+            }
+        }
+
+//        while (!q.isEmpty()) {
+//            if (q.peek()[1] == pq.peek()) {
+//                if (q.poll()[0] == M) {
+//                    System.out.println(cnt++);
+//                    break;
+//                }
+//                else
+//                    q.add(q.poll());
+//            }
+//        }
+    }
+}
