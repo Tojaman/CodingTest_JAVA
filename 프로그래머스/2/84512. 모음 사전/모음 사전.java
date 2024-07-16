17017cf1fcf0e0d65c5ead13eec29f6092c718cb
조형준 <89668546+Tojaman@users.noreply.github.com>
2024-07-16 23:59:17 +0900

    [level 2] Title: 모음 사전, Time: 70.19 ms, Memory: 85.3 MB -BaekjoonHub

--- 프로그래머스/2/84512. 모음 사전/모음 사전.java
+++ 프로그래머스/2/84512. 모음 사전/모음 사전.java
@@ -1,22 +1,20 @@
-/** 문제 설명
-'A', 'E', 'I', 'O', 'U'으로 5자리 이하 단어가 사전 순으로 정렬되어 있다.
-이때 word가 몇 번째로 정렬되어 있는지 반환하라.
-*/
+// /** 문제 설명
+// 'A', 'E', 'I', 'O', 'U'으로 5자리 이하 단어가 사전 순으로 정렬되어 있다.
+// 이때 word가 몇 번째로 정렬되어 있는지 반환하라.
+// */
 
-/** 문제 풀이
-1. 모든 단어 조합 만들어 배열에 삽입
-2. 배열을 사전 순서대로 정렬
-3. 배열 index == 단어의 순서
-*/
+// /** 문제 풀이
+// 1. 모든 단어 조합 만들어 배열에 삽입
+// 2. 배열을 사전 순서대로 정렬
+// 3. 배열 index == 단어의 순서
+// */
 
-/** 단어 조합 만들기
-- 재귀로 만들자
-- 5글자로 제한하자
-- 중복이 되지 않도록 하자
-*/
-import java.util.ArrayList;
-import java.util.Arrays;
-import java.util.Collections;
+// /** 단어 조합 만들기
+// - 재귀로 만들자
+// - 5글자로 제한하자
+// - 중복이 되지 않도록 하자
+// */
+import java.util.*;
 // class Solution {
 //     ArrayList<String> combination = new ArrayList<>();
 //     String[] vowels = {"A", "E", "I", "O", "U"};
@@ -26,7 +24,12 @@ import java.util.Collections;
 //         }
 //         // Arrays.sort(combination);
 //         Collections.sort(combination);
-//         return combination.indexOf(word) + 1;
+//         // for (int i = 0; i < conbination.size(); i++) {
+//         //     if (combination.get(i).equals(word))
+//         //         return i+1;
+//         // }
+//         return binarySearch(word) + 1;
+//         // return combination.indexOf(word) + 1;
         
 //     }
     
@@ -34,19 +37,53 @@ import java.util.Collections;
 //         String newVowel = vowel + addVowel;
 //         if (newVowel.length() > 5)
 //             return;
-//         if (!combination.contains(newVowel))
-//             combination.add(newVowel);
+//         // if (!combination.contains(newVowel)) // 어차피 중복은 발생하지 않기 때문에 필요 없음.(없애면 시간 많이 줄어듬)
+//         combination.add(newVowel);
 //         for (String vo : vowels) {
 //             dfs(newVowel, vo);
 //         }
 //     }
     
+//     private int findIndex(String word) {
+//         for (int i = 0; i < combination.size(); i++) {
+//             if (combination.get(i).equals(word)) {
+//                 return i;
+//             }
+//         }
+//         return -1; // 찾지 못한 경우
+//     }
+    
+//     // 이진 탐색을 사용하여 단어의 인덱스를 찾는 메서드
+//     private int binarySearch(String word) {
+//         int low = 0;
+//         int high = combination.size() - 1;
+
+//         while (low <= high) {
+//             int mid = (low + high) / 2;
+//             int cmp = combination.get(mid).compareTo(word);
+
+//             if (cmp == 0) {
+//                 return mid;
+//             } else if (cmp < 0) {
+//                 low = mid + 1;
+//             } else {
+//                 high = mid - 1;
+//             }
+//         }
+
+//         return -1; // 단어를 찾지 못한 경우
+//     }
 // }
 
 class Solution {
+    List<String> list = new ArrayList<>();
+    void dfs(String str, int len) {
+        if(len > 5) return;
+        list.add(str);
+        for(int i = 0; i < 5; i++) dfs(str + "AEIOU".charAt(i), len + 1);
+    }
     public int solution(String word) {
-        int answer = 0, per = 3905;
-        for(String s : word.split("")) answer += "AEIOU".indexOf(s) * (per /= 5) + 1;
-        return answer;
+        dfs("", 0);
+        return list.indexOf(word);
     }
 }
