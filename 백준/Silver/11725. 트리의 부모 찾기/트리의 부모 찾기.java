bad04879e1d2a98daef17cd68f3369b8ed7e7689
조형준 <89668546+Tojaman@users.noreply.github.com>
2024-08-12 21:49:36 +0900

    [Silver II] Title: 트리의 부모 찾기, Time: 1404 ms, Memory: 80728 KB -BaekjoonHub

--- 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
+++ 백준/Silver/11725. 트리의 부모 찾기/트리의 부모 찾기.java
@@ -3,22 +3,23 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
+import java.util.ArrayList;
 
 public class Main {
-    static LinkedList<Integer>[] graph;
+    static ArrayList<Integer>[] graph;
     static boolean[] visited;
     static int[] parents;
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         int N = Integer.parseInt(br.readLine());
-        graph = new LinkedList[N+1];
+        graph = new ArrayList[N+1];
         parents = new int[N+1];
         visited = new boolean[N+1];
 
         for (int i = 1; i <= N; i++)
-            graph[i] = new LinkedList<>();
-        
+            graph[i] = new ArrayList<>();
+
         for (int i = 0; i < N-1; i++) {
             StringTokenizer st = new StringTokenizer(br.readLine());
             int a = Integer.parseInt(st.nextToken());
