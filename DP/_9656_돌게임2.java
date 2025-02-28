021bae2d2308521dbb09d627926ab5207cddcd87
Tojaman <zzoon5020@naver.com>
2025-02-28 12:47:31 +0900

    2월 28일 DP 백준 실버5(수정)

--- DP/_9656_돌게임2.java
+++ DP/_9656_돌게임2.java
@@ -9,21 +9,27 @@ public class _9656_돌게임2 {
         BufferedReader br = new BufferedReader((new InputStreamReader(System.in)));
         
         /** 풀이 방법 - DP
-         * dp[n]: dp 배열에는 패자를 저장한다.
+         * dp[n]: dp 배열에는 승자를 저장한다.
          * 돌은 1개 or 3개를 가져갈 수 있으므로 dp[현재-1]과 dp[현재-3] 사람의 반대 사람이 승자가 된다.
          */
         int n = Integer.parseInt(br.readLine());
 
-        if (n == 1 || n == 3) System.out.println("CY");
-        if (n == 2) System.out.println("SK");
+        if (n == 1 || n == 3) {
+            System.out.println("CY");
+            return;
+        }
+        if (n == 2) {
+            System.out.println("SK");
+            return;
+        }
 
         String[] dp = new String[n+1];
         dp[1] = "CY";
         dp[2] = "SK";
-        dp[1] = "CY";
+        dp[3] = "CY";
 
         for (int i = 4; i <= n; i++) {
-            if (dp[i-1] == "SK" || dp[i-3] == "SK")
+            if (dp[i-1].equals("SK") || dp[i-3].equals("SK"))
                 dp[i] = "CY";
             else
                 dp[i] = "SK";
