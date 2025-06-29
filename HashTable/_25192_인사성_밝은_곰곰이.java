8ccd82adcc69b3ed476f4b38dd5bceedea10cf46
tojaman <zzoon5020@naver.com>
2025-06-29 16:18:27 +0900

    (복습)6월 29일 HashMap 백준 실버4(size(), clear())

--- HashTable/_25192_인사성_밝은_곰곰이.java
+++ HashTable/_25192_인사성_밝은_곰곰이.java
@@ -6,32 +6,38 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 
 /**
- * 채팅방 입장할 때 각 유저는 첫 채팅은 무조껀 이모티콘 사용
- * 이모티콘 사용 횟수 구하기
- * ENTER: 채팅방 입장
- * 나머지: 닉네임(숫자 or 영어 대소문자)
- * 
+ * 곰곰 이모티콘 사용 횟수
+ * ENTER: 입장 / 외: 닉네임
+ * 첫 채팅은 이모티콘 -> 첫 채팅을 친 사람의 수를 구하시오
+ */
+
+/** HashSet 메소드
+ * add(), remove(), contains()
+ * equals(): HashSet 객체 끼리 비교(순서 상관X)
+ * size(): 크기
+ * clear(): 전부 삭제
  */
 public class _25192_인사성_밝은_곰곰이 {
     public static void main(String[] arge) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        /**
+         * ENTER가 나오면 이후에 나오는 닉네임 수 카운팅
+         * 이때 닉네임이 두 번 이상 나올 경우 카운팅하지 X
+         */
         int n = Integer.parseInt(br.readLine());
-        HashSet<String> hs = new HashSet<>(); // 이모티콘 저장 Set
-        int result = 0; // 이모티콘 사용 횟수
-
-        // "ENTER"가 입력되면 전까지 입력된 이모티콘의 수를 세서 result에 저장장
-        for (int i = 0; i < n; i ++) {
-            String input = br.readLine();
-            if (input.equals("ENTER")) {
-                result += hs.size();
-                hs.clear();
+        HashSet<String> hs = new HashSet<>();
+        int cnt = 0;
+        for (int i = 0; i < n; i++) {
+            String chat = br.readLine();
+            if (chat.equals("ENTER")) {
+                cnt += hs.size(); // 외우기
+                hs.clear(); // 외우기
                 continue;
             }
-            hs.add(input);
+            if (!hs.contains(chat))
+                hs.add(chat);
         }
-
-        // 순회 후 남은 이모티콘 사용 횟수를 result에 더해서 출력력
-        System.out.println(result + hs.size());
+        System.out.println(cnt + hs.size());
     }
 }
