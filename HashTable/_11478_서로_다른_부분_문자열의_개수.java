fecdb5b3a0d490135f62bff80dd193ade589c8b3
tojaman <zzoon5020@naver.com>
2025-06-29 17:36:06 +0900

    (복습)6월 29일 HashSet(String) 백준 실버4(substring(int startIndex, endIndex(불포함)))

--- HashTable/_11478_서로_다른_부분_문자열의_개수.java
+++ HashTable/_11478_서로_다른_부분_문자열의_개수.java
@@ -6,8 +6,9 @@ import java.io.InputStreamReader;
 import java.util.HashSet;
 
 /** 부분 문자열
- * substring(int beginIndex)
- * substring(int beginIndex, int endIndex)
+ * substring(int beginIndex): beginindex(포함)부터 끝까지 부분 문자열
+ * substring(int beginIndex, int endIndex): beginindex(포함) ~ endIndex(불포함) 부분 문자열
+ * endIndex는 String 객체의 length()까지 가능능
  */
 
 public class _11478_서로_다른_부분_문자열의_개수 {
@@ -15,22 +16,15 @@ public class _11478_서로_다른_부분_문자열의_개수 {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
         String s = br.readLine();
+
         HashSet<String> hs = new HashSet<>();
-
-        // int beginIndex = 0;
-        // while (beginIndex < s.length()) {
-        //     int endIndex = beginIndex + 1;
-        //     while (endIndex <= s.length()) {
-        //         hs.add(s.substring(beginIndex, endIndex++));
-        //     }
-        //     beginIndex++;
-        // }
-
-        for (int i = 0; i < s.length(); i++) {
-            for (int j = i+1; j <= s.length(); j++) {
-                hs.add(s.substring(i, j));
+        for (int i = 1; i <= s.length(); i++) { // 부분 문자열 크기(1 ~ 5)
+            for (int j = 0; j < s.length(); j++) { // 부분 문자열 시작점 (0 ~ 4)
+                if (j + i > s.length()) continue;
+                hs.add(s.substring(j, j + i));
             }
         }
+
         System.out.println(hs.size());
     }
 }
