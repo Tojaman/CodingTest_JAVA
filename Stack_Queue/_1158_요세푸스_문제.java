4536267af5f61717ec52635a552756d5f3bdb570
tojaman <zzoon5020@naver.com>
2025-06-30 18:46:37 +0900

    (복습)6월 30일 Queue 백준 실버4

--- Stack_Queue/_1158_요세푸스_문제.java
+++ Stack_Queue/_1158_요세푸스_문제.java
@@ -3,54 +3,79 @@ package Stack_Queue;
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStreamReader;
-import java.lang.reflect.Array;
-import java.util.*;
-
+import java.util.ArrayList;
+import java.util.LinkedList;
+import java.util.Queue;
+import java.util.StringTokenizer;
 /**
- * 1 2 3 4 5 6 7
- * 4 5 6 7 1 2
- * 7 1 2 4 5
- * 4 5 7 1
- * 1 4 5
- * 1 4
- * 3 6 2 7 5 1 4
- */
-
-/**
- * k-1까지 offer -> poll하고 k번째 값 저장
+ * n = 총 인원(1 ~ n)
+ * k = 제거되는 사람 번호
+ * 첫 번째 인원은 제거된 사람 바로 다음 사람
  */
 public class _1158_요세푸스_문제 {
     public static void main(String[] args) throws IOException {
+        
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
-
-        Queue<Integer> q = new LinkedList<>();
         StringTokenizer st = new StringTokenizer(br.readLine());
-        int n = Integer.parseInt(st.nextToken()); // 사람 수
-        int k = Integer.parseInt(st.nextToken()); // k번째 사람 제거
 
-        for (int i = 1; i <= n; i++) q.offer(i);
+        int n = Integer.parseInt(st.nextToken());
+        int k = Integer.parseInt(st.nextToken());
 
-        ArrayList<Integer> arr = new ArrayList<>();
-        while(true) {
-            for (int i = 0; i < k; i++) {
-                if (i == k - 1) {
-                    arr.add(q.poll());
-                    break;
-                }
-                q.offer(q.poll());
-            }
-            if (arr.size() == n) break;
+        Queue<Integer> q = new LinkedList<>(); 
+        for (int i = 1; i <= n; i++) {
+            q.offer(i);
         }
 
         StringBuilder sb = new StringBuilder();
         sb.append("<");
-        for (int i = 0; i < arr.size(); i++) {
-            sb.append(arr.get(i));
-            if (i != arr.size()-1) {
-                sb.append(", ");
+        while (!q.isEmpty()) {
+            for (int i = 1; i <= k; i++) {
+                if (i == k) {
+                    sb.append(q.poll() + ", ");
+                    break;
+                }
+
+                q.offer(q.poll());
+                
             }
         }
+        sb.delete(sb.length() - 2, sb.length());
         sb.append(">");
-        System.out.println(sb);
+        System.out.print(sb.toString());
     }
+
+    // 처음 푼 방식
+    // public static void main(String[] args) throws IOException {
+    //     BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+    //     Queue<Integer> q = new LinkedList<>();
+    //     StringTokenizer st = new StringTokenizer(br.readLine());
+    //     int n = Integer.parseInt(st.nextToken()); // 사람 수
+    //     int k = Integer.parseInt(st.nextToken()); // k번째 사람 제거
+
+    //     for (int i = 1; i <= n; i++) q.offer(i);
+
+    //     ArrayList<Integer> arr = new ArrayList<>();
+    //     while(true) {
+    //         for (int i = 0; i < k; i++) {
+    //             if (i == k - 1) {
+    //                 arr.add(q.poll());
+    //                 break;
+    //             }
+    //             q.offer(q.poll());
+    //         }
+    //         if (arr.size() == n) break;
+    //     }
+
+    //     StringBuilder sb = new StringBuilder();
+    //     sb.append("<");
+    //     for (int i = 0; i < arr.size(); i++) {
+    //         sb.append(arr.get(i));
+    //         if (i != arr.size()-1) {
+    //             sb.append(", ");
+    //         }
+    //     }
+    //     sb.append(">");
+    //     System.out.println(sb);
+    // }
 }
