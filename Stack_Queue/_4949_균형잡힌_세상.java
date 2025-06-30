9c2742bac7e69e6cdb5b8d98e7548a2ad4dceec2
tojaman <zzoon5020@naver.com>
2025-06-30 18:13:00 +0900

    (복습)6월 30일 Stack 백준 실버4

--- Stack_Queue/_4949_균형잡힌_세상.java
+++ Stack_Queue/_4949_균형잡힌_세상.java
@@ -5,65 +5,59 @@ import java.io.IOException;
 import java.io.InputStreamReader;
 import java.util.Stack;
 
-/** 조건
- * 알파벳, 공백, (), []만 사용 가능 && 마지막은 .으로 끝남(종료 조건)
- * 소괄호는 소괄호끼리, 대괄호는 대괄호끼리
+/**
+ * (), [] 균형 맞는지 검증 -> 맞으면 yes, 안맞으면 no
  */
 
 /** 풀이 방법
- * 열린 괄호가 나오면 스택에 push하고 같은 종류의 닫힌 괄호가 나오면 pop한다.
- * 스택에 괄호가 남으면 no
- * 스택에 짝이 없는 닫힌 괄호가 나오면 no
+ * 여는 괄호가 나오면 Stack에 넣고 닫는 괄호가 나오면 짝 검증
  */
 
-/** 실수
- * 각 문자를 char가 아닌 String으로 받음 -> 만약 "(str home"이라는 문자열이 있다면 "(str"을 한 덩어리로 받음
- */
 public class _4949_균형잡힌_세상 {
     public static void main(String [] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
-
-        while(true) {
+        while (true) {
             Stack<Character> stack = new Stack<>();
-            String result = "yes";
-
             String input = br.readLine();
-            // 종료 조건
             if (input.equals(".")) break;
-            char[] chars = input.toCharArray();
 
-            // 문자열이 끝날 때까지 반복
-            for (char ch : chars) {
-                // 열린 괄호
+            String result = "yes";
+            for (int i = 0; i < input.length(); i++) {
+                char ch = input.charAt(i);
+
+                if (ch == '.') break;
+
+                // 왼쪽 괄호면 넣기
                 if (ch == '(' || ch == '[') {
                     stack.push(ch);
                     continue;
                 }
-                // 닫힌 괄호
+                // 오른쪽 괄호면 짝 검증
                 if (ch == ')' || ch == ']') {
-                    if (stack.isEmpty()) { // 스택이 비어있으면 바로 오류 처리
+                    if (stack.isEmpty()) {
                         result = "no";
                         break;
                     }
-                    char stNow = stack.pop();
-                    // 열린 괄호와 닫힌 괄호의 짝이 안 맞으면 no 출력 후 종료
-                    if (ch == ')' && stNow != '(') {
-                        result = "no";
-                        break;
+
+                    char pop = stack.pop();
+                    if (ch == ')') {
+                        if (pop != '(') {
+                            result = "no";
+                            break;
+                        }
                     }
-                    if (ch == ']' && stNow != '[') {
-                        result = "no";
-                        break;
+                    else {
+                        if (pop != '[') {
+                            result = "no";
+                            break;
+                        }
                     }
                 }
             }
-            // 스택이 비어있지 않은 경우
-            if (!stack.isEmpty()) {
-                result = "no";
-            }
+            // ⭐조건⭐
+            if (!stack.isEmpty()) result = "no";
             System.out.println(result);
         }
-
     }
 }
