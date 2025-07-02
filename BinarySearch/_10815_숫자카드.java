5901c1e337f492fbd5a5da31a065137d7b9bfd60
Tojaman <zzoon5020@naver.com>
2025-07-02 13:43:43 +0900

    (복습)7월 2일 BinarySearch 백준 실버5

--- BinarySearch/_10815_숫자카드.java
+++ BinarySearch/_10815_숫자카드.java
@@ -4,10 +4,59 @@ import java.io.InputStreamReader;
 import java.util.Arrays;
 import java.util.StringTokenizer;
 
+// 25.07.02
+/** 이진 탐색 시간 복잡도
+ * 조건: 2초
+ * 수 범위: 2000만
+ * 정렬: O(NlogN) - 1000만
+ * 이진 탐색: O(MlogN) - 1000만 * 1000만
+ * 총 시간 복잡도: O(NlogN+MlogN)
+*/
+public class _10815_숫자카드 {
+    static int[] cards;
+    public static void main(String[] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        int n = Integer.parseInt(br.readLine());
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        cards = new int[n];
+        for (int i = 0; i < n; i++) {
+            cards[i] = Integer.parseInt(st.nextToken());
+        }
+        Arrays.sort(cards);
+
+        int m = Integer.parseInt(br.readLine());
+        st = new StringTokenizer(br.readLine());
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < m; i++) {
+            int verification = Integer.parseInt(st.nextToken());
+            sb.append(binary(0, cards.length-1, verification) + " ");
+        }
+
+        System.out.print(sb.toString().trim());
+    }
+    public static int binary(int left, int right, int target) {
+        int mid = (left + right) / 2;
+        
+        if (left > right) return 0;
+
+        if (target > cards[mid]) {
+            return binary(mid + 1, right, target);
+        } else if(target < cards[mid]) {
+            return binary(left, mid - 1, target);
+        } else {
+            return 1;
+        }
+        
+    }
+}
+
+// 이전 풀이
 /** 이진 탐색
  * 정렬된 배열의 중간 값과 비교하여 크다면 오른쪽을 탐색하고, 작다면 왼쪽을 탐색한다.
  * 목표 값을 찾을 때까지 위 과정을 반복하여 찾으면 1을 저장하고 마지막까지 찾지 못한다면 0을 저장한다.
 */
+/**
 public class _10815_숫자카드 {
     public static void main(String[] args) throws IOException {
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
@@ -67,5 +116,5 @@ public class _10815_숫자카드 {
     //         return binarySearchIterative(arr, target, low, mid - 1);
     //     }
     // }
-
 }
+*/
