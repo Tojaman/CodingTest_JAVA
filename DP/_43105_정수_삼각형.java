065b2c3754ae97e46097f200476b8413b86e0770
tojaman <zzoon5020@naver.com>
2025-07-31 02:45:34 +0900

    7월 31일 DP 프로그래머스 Lv3(복습)

--- DP/_43105_정수_삼각형.java
+++ DP/_43105_정수_삼각형.java
@@ -1,6 +1,38 @@
 package DP;
 
+/* 복습2 */
+class _43105_정수_삼각형 {
+    public int solution(int[][] triangle) {
+        int height = triangle.length;
+        int width = triangle[height-1].length;
+        int[][] memo = new int[height][width];
+        memo[0][0] = triangle[0][0];
+        
+        int result = 0;
+        for (int i = 1; i < triangle.length; i++) {
+            for (int j = 0; j < triangle[i].length; j++) {
+                
+                if (j == 0) { // 왼쪽(위에 값 + 자신 값)
+                    memo[i][j] = memo[i-1][j] + triangle[i][j];
+                }
+                if (i == j) { // 오른쪽(위에 값 + 자신 값)
+                    memo[i][j] = memo[i-1][j-1] + triangle[i][j];
+                }
+                
+                if (j != 0 && i != j) { // 위에 왼쪽, 오른쪽 값 중 큰 값 + 자신 값
+                    memo[i][j] = Math.max(memo[i-1][j-1], memo[i-1][j]) + triangle[i][j];
+                }
+                
+                if (i == height-1) {
+                    result = Math.max(result, memo[i][j]);
+                }
+            }
+        }
+        return result;
+    }
+}
 
+/* 첫 도전 
 class _43105_정수_삼각형 {
     public int solution(int[][] triangle) {
 
@@ -20,4 +52,4 @@ class _43105_정수_삼각형 {
         return memo[0][0];
     }
 }
-
+*/
