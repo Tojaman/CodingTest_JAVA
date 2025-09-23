a204c7e5deddc22632289a9bbfe140fe4608813b
tojaman <zzoon5020@naver.com>
2025-09-24 05:11:29 +0900

    9월 24일 DP 프로그래머스 Lv.2

--- DP/_12913_땅따먹기_DP.java
+++ DP/_12913_땅따먹기_DP.java
@@ -8,9 +8,32 @@ package DP;
 - 밝은 땅 점수의 합이 최대가 되도록 하기
 **/
 
+import java.util.Arrays;
+
+class Solution {
+    int solution(int[][] land) {
+        int answer = 0;
+        int rowSize = land.length;
+        
+        // 열이 4개로 고정이니깐 이렇게 가능
+        // 총 400,000번 순환
+        for (int i = 1; i < rowSize; i++) {
+            land[i][0] += Math.max(land[i-1][1],Math.max(land[i-1][2],land[i-1][3]));
+            land[i][1] += Math.max(land[i-1][0],Math.max(land[i-1][2],land[i-1][3]));
+            land[i][2] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][3]));
+            land[i][3] += Math.max(land[i-1][0],Math.max(land[i-1][1],land[i-1][2]));
+        }
+        
+        for (int j = 0; j < 4; j++) answer = Math.max(answer, land[rowSize-1][j]);
+
+        return answer;
+    }
+}
+
 /** 풀이 방법: DP(Top Down)
 dp[][] 배열의 각 칸마다 조건에 맞은 가장 큰 값을 저장
 **/
+/**
 class Solution {
     int solution(int[][] land) {
         int answer = 0;
@@ -29,13 +52,14 @@ class Solution {
                         dp[i][j] = Math.max(land[i][j] + dp[i-1][k], dp[i][j]);
                     }
                 }
-                answer = Math.max(answer, dp[i][j]);
             }
         }
+        for (int j = 0; j < 4; j++) answer = Math.max(answer, dp[rowSize-1][j]);
 
         return answer;
     }
 }
+**/
 
 /** 풀이 방법: Greedy -> 당장 예시의 두 번째 행의 8을 10으로 바꾸면 최고값이 아님
 1. 행을 순서대로 내려옴
