2e70286c246b74a3c5b0617f15178e0c7f002203
tojaman <zzoon5020@naver.com>
2026-02-08 01:35:36 +0900

    2026.02.08 DFS 백준 실버2

--- DFS/_11724_연결_요소의_개수.java
+++ DFS/_11724_연결_요소의_개수.java
@@ -5,6 +5,63 @@ import java.io.InputStreamReader;
 import java.util.LinkedList;
 import java.util.StringTokenizer;
 
+
+/* 풀이 방법
+끝까지 탐색해야 하므로 DFS
+*/
+// 복습: 2026.02.08
+// 구현 시간: 20분 (초기화 안함;;)
+public class _11724_연결_요소의_개수 {
+    static boolean[] visited;
+    static LinkedList<Integer>[] graph;
+    static int n, m;
+    public static void main(String [] args) throws IOException {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+
+        StringTokenizer st = new StringTokenizer(br.readLine());
+        n = Integer.parseInt(st.nextToken());
+        m = Integer.parseInt(st.nextToken());
+
+        graph = new LinkedList[n+1];
+        visited = new boolean[n+1];
+
+        // 💥초기화 반드시 해야 함
+        for (int i = 1; i <= n; i++) {
+            graph[i] = new LinkedList<>();
+        }
+
+        for (int i = 0; i < m; i++) {
+            st = new StringTokenizer(br.readLine());
+            int u = Integer.parseInt(st.nextToken());
+            int v = Integer.parseInt(st.nextToken());
+            
+            graph[u].add(v);
+            graph[v].add(u);
+        }
+
+        int cnt = 0;
+        for (int i = 1; i <= n; i++) {
+            if (!visited[i]) {
+                dfs(i);
+                cnt++;
+            }
+        }
+        System.out.println(cnt);
+    }
+
+    static public void dfs(int node) {
+        visited[node] = true;
+
+        // 노드를 돌면서 방문하지 않은 노드 방문
+        for (int next : graph[node]) {
+            if (!visited[next]) {
+                dfs(next);
+            }
+        }
+    }
+}
+
+
 /** 복습 풀이 - 25.07.14
  * 1. LinkedList로 그래프 그리기
  * 2. DFS 탐색을 통해 연결 요소의 개수 구하기
